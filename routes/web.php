43e378ad98dc704783d26b4ba9fbcc4709677d7c
geumaxh <maxeneduavepereira325@gmail.com>
2026-05-07 18:52:14 +0800

    added product routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductsController;
 
 Route::view('/', 'welcome');
 
@@ -25,3 +26,8 @@ Route::middleware('admin')->group(function () {
 Route::resource('users', UserController::class)->middleware('admin');
 
 require __DIR__ . '/auth.php';
+
+// Product routes - only logged in users can access
+Route::middleware(['auth'])->group(function () {
+    Route::resource('products', ProductsController::class);
+});
